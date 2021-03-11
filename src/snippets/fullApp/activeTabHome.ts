72aca901f444c86765aa1d5baea4067861666b2e
Harsh <harsh.rathi@thoughtspot.com>
2021-03-11 19:15:49 +0530

    - Added test cases for Full App

---
+++ src/snippets/fullApp/activeTabHome.ts
@@ -0,0 +1,38 @@
+export default `
+// Import ThoughtSpot SDK
+import {
+	init,
+	AppEmbed,
+	Action,
+	EventType,
+	AuthType
+} from "@thoughtspot/embed-sdk";
+import "./styles.css";
+// Initialize embed configuration
+init({
+	thoughtSpotHost: '<%=tshost%>',
+	authType: AuthType.None
+});
+// Instantiate class for embedding a pinboard
+const embed = new AppEmbed("#embed", {
+	frameParams: {
+		width: 1280,
+		height: 720
+	},
+	showPrimaryNavbar: true,
+});
+embed
+	// Register event listeners
+	.on("init", showLoader)
+	.on("load", hideLoader)
+	.render({
+		pageId: "home",
+	});
+// Functions to show and hide a loader while iframe loads
+function showLoader() {
+	document.getElementById("loader").style.display = "block";
+}
+function hideLoader() {
+	document.getElementById("loader").style.display = "none";
+}
+`;
---
+++ src/snippets/fullApp/activeTagPinboards.ts
@@ -0,0 +1,37 @@
+export default `
+// Import ThoughtSpot SDK
+import {
+    init,
+    AppEmbed,
+    Action,
+    EventType,
+    AuthType
+} from "@thoughtspot/embed-sdk";
+import "./styles.css";
+// Initialize embed configuration
+init({
+	thoughtSpotHost: '<%=tshost%>',
+	authType: AuthType.None
+});
+// Instantiate class for embedding a pinboard
+const embed = new AppEmbed("#embed", {
+	frameParams: {
+		width: 1280,
+		height: 720
+	},
+});
+embed
+	// Register event listeners
+	.on("init", showLoader)
+	.on("load", hideLoader)
+	.render({
+		pageId: "pinboards",
+	});
+// Functions to show and hide a loader while iframe loads
+function showLoader() {
+  	document.getElementById("loader").style.display = "block";
+}
+function hideLoader() {
+  	document.getElementById("loader").style.display = "none";
+}
+`;
